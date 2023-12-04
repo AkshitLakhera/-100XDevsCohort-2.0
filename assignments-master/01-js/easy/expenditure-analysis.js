a54273865eb903e023ba025af3ba5d16d4182a7a
“[Akshit <akshitlakhera14@gmail.com>
2023-12-05 01:53:13 +0530

    Done with expenditure analysis

--- assignments-master/01-js/easy/expenditure-analysis.js
+++ assignments-master/01-js/easy/expenditure-analysis.js
@@ -6,7 +6,39 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+const categoryTotal = {};
+for (let i = 0 ; i<transactions.length;i++){
+  const currentCatory =transactions[i].category;
+  if (!categoryTotal[currentCatory]){
+    categoryTotal[currentCatory] = transactions[i].price;
+  }else{
+    categoryTotal[currentCatory]+= transactions[i].price;
+  }
 }
 
+  // Create an array of objects with "category" and "totalSpent" properties
+const resultArray = Object.keys(categoryTotal).map(category => ({
+  category,
+  totalSpent: categoryTotal[category],
+}));
+
+console.log(resultArray);
+return resultArray;
+}
+// You can use Object.keys() to get an array of object keys and Object.values() to get an array of object values. Then, you can iterate over the keys and use them to access the corresponding values.
+// const category = Object.keys(categoryTotal)
+// const price = Object.values(categoryTotal)
+// const originalTotal = []
+// for(let i =0;i<categoryTotal.length,i++;i++){
+//   originalTotal = 
+// }
+// }
+
 module.exports = calculateTotalSpentByCategory;
+
+
+// expected
+// [{"category": "Food", "totalSpent": 30}, {"category": "Clothing", "totalSpent": 40}, {"category": "Electronics", "totalSpent": 30}];
+
+// my code
+// {"Clothing": 40, "Electronics": 30, "Food": 30}
