b21415b3e87411980374006f3c3133f89c2c1825
“[Akshit <akshitlakhera14@gmail.com>
2023-12-05 12:07:02 +0530

    Learning  from the prob lem added

--- assignments-master/01-js/easy/expenditure-analysis.js
+++ assignments-master/01-js/easy/expenditure-analysis.js
@@ -4,7 +4,9 @@
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 */
-
+// My learning
+// You can use Object.keys() to get an array of object keys and Object.values() to get an array of object values. Then, you can iterate over the keys and use them to access the corresponding values.
+//  The map() method in JavaScript iterates over each element of an array and creates a new array by applying a provided function to each element. It does not mutate the original array; instead, it returns a new array with the results of applying the provided function to each element.
 function calculateTotalSpentByCategory(transactions) {
 const categoryTotal = {};
 for (let i = 0 ; i<transactions.length;i++){
@@ -25,7 +27,6 @@ const resultArray = Object.keys(categoryTotal).map(category => ({
 console.log(resultArray);
 return resultArray;
 }
-// You can use Object.keys() to get an array of object keys and Object.values() to get an array of object values. Then, you can iterate over the keys and use them to access the corresponding values.
 // const category = Object.keys(categoryTotal)
 // const price = Object.values(categoryTotal)
 // const originalTotal = []
