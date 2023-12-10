06767e53afe6ee7846a201009b6176015bbd0723
“[Akshit <akshitlakhera14@gmail.com>
2023-12-10 12:19:12 +0530

    added counter.js without set interval

---
+++ assignments-master/week-2/01-async-js/easy/easy solutions/counterWithoutSetinterval.js
@@ -0,0 +1,6 @@
+// Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
+let count = 0;
+setTimeout(function(){
+    count++;
+    console.log(count);
+},1000)
