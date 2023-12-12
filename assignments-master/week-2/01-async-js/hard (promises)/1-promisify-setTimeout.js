92efc76669a1b57111ceec86fc769f822611f40e
“[Akshit <akshitlakhera14@gmail.com>
2023-12-12 11:29:58 +0530

    First hard problem is done

--- assignments-master/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ assignments-master/week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -1,6 +1,19 @@
 /*
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
 */
+function wait(duration) {
 
-function wait(n) {
+    const resolved = new Promise((resolve)=>{
+        setTimeout(() => {
+            resolve('Promise resolved')
+        }, duration);
+    })
+       
+    
+
+    return resolved;
 }
+
+wait(2000).then((data)=>{
+    console.log(data)
+})
