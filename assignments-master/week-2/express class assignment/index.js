cad6367e0de3bf39fb56f1002c7e979e04fb6c57
“[Akshit <akshitlakhera14@gmail.com>
2023-12-16 22:54:28 +0530

    Express js practice folder

---
+++ assignments-master/week-2/express class assignment/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app =express();
+const users = [{
+    name :'john',
+    kidneys: [{
+        healthy:false
+    }]
+}]
+// To get json  body on server  you  have to use app.use(express.json()) as body parser
+app.use(express.json()); 
+app.get("/",function(req,res){
+    // write logic
+    const johnKidneys= users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    // logic
+    let healthykidneys = 0
+    for(let i = 0;i<johnKidneys.length;i++){
+        if(johnKidneys[i].healthy){
+            healthykidneys= healthykidneys+1;
+        }
+    }
+const unhealthyKidneys = numberOfKidneys-healthykidneys;
+res.json({
+    numberOfKidneys,
+    healthykidneys,
+    unhealthyKidneys
+})
+})
+app.post("/",function(req,res) {
+    const  isHealthy = req.body.isHealthy;        
+    users[0].kidneys.push({
+        healthy:isHealthy
+    });
+     res.json ({
+        msg:"done"
+     })
+})
+app.listen(3000);
