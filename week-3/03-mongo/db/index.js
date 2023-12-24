48c8322e873802695b68b9998b53d548d7d43ee9
“[Akshit <akshitlakhera14@gmail.com>
2023-12-24 13:10:01 +0530

    Added database code

--- week-3/03-mongo/db/index.js
+++ week-3/03-mongo/db/index.js
@@ -1,19 +1,67 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://admin:Vihaan12%40@cluster0.jpfavz7.mongodb.net/coursewebsite');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
-    // Schema definition here
-});
+    username :{
+        type:String,
+        required:true
+    },
+    password : {
+        type:String,
+        required:true,
+        length:8,
+    },
+    courses : [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Course,'
+    }
+    ]});
 
 const UserSchema = new mongoose.Schema({
-    // Schema definition here
+    username:{
+        type:String,
+        required:true
+    },
+    password :{
+        type:String,
+        required:true
+    },
+    purchasedCourses :[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
-    // Schema definition here
+    courseId :{
+        type:Number,
+        required:true,
+        unique:true    },
+    title : {
+        type:String,
+        required:true,
+        unique:true
+    },
+    description:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    imageLink:{
+        type:String,
+        required:true
+    },
+    published:{
+        type:Boolean,
+        required:true
+    },
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
