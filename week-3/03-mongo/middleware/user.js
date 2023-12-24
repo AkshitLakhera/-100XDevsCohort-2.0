bbd757423b52a8871a7fa90c0305c86d3f868cd3
“[Akshit <akshitlakhera14@gmail.com>
2023-12-24 23:12:34 +0530

    Added user middleware

--- week-3/03-mongo/middleware/user.js
+++ week-3/03-mongo/middleware/user.js
@@ -1,6 +1,15 @@
-function userMiddleware(req, res, next) {
+const {User} = require('../db/');
+async function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const {username,password} = req.header;
+    const verifiedusername = await User.findOne({username:username,password:password});
+    if(!verifiedusername){
+        res.status(404).send("Unauthorized user");
+    }
+    else{
+        next();
+    }
 }
 
 module.exports = userMiddleware;
