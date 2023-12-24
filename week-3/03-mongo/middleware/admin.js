4687b9fa9d67bbe841550eb77bc958dabf6745b8
“[Akshit <akshitlakhera14@gmail.com>
2023-12-24 17:19:41 +0530

    Done with admin.js

--- week-3/03-mongo/middleware/admin.js
+++ week-3/03-mongo/middleware/admin.js
@@ -1,8 +1,16 @@
+// Accessing Admin model from database
+const {Admin} = require('../db/');
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
-    
+    const {username,password} = req.header;
+    const verifiedusername = await Admin.findOne({username:username,password:password});
+    if(!verifiedusername) {
+        res.status(404).send("Unauthorized user")
+    } else {
+        next();
+    }
 }
 
 module.exports = adminMiddleware;
