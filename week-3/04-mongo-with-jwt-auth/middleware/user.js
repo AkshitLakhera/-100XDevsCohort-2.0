f0d236a8e20550cea483fdba62074d867cf7795d
“[Akshit <akshitlakhera14@gmail.com>
2023-12-27 22:39:10 +0530

    Added user middleware

--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,24 @@
+const jwt = require("jsonwebtoken");
 function userMiddleware(req, res, next) {
     // Implement user auth logic
     // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    const token  = req?.headers?.authorization?.split(" ")[1];
+    if (!token) {
+         return res.status(401).send({ message: "Unauthorized" });
+      } 
+      try {
+        jwt.verify(token, process.env.JWT_KEY,(err,data)=> {
+            if(err){
+                 return res.status(401).send({ message: "Unauthorized" });
+            } else {
+                req._id = data._id;
+                next();
+            }
+        })
+
+      } catch(e) {
+        res.status(500).send({ message: "Error in verying token", error: error })
+      }
 }
 
 module.exports = userMiddleware;
