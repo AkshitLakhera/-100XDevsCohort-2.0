523b1dc7645e4994cd0d50c9b5ac4f89a30a7131
“[Akshit <akshitlakhera14@gmail.com>
2023-12-27 20:41:20 +0530

    Done with database

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,58 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username :{type:String,  unique :true , required :true},
+    password :{type:String,required: true},
+    courses : [{type: mongoose.Schema.Types.ObjectId, ref: "Course" }]
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username:{
+        type:String,
+        required:true
+    },
+    password :{
+        type:String,
+        required:true
+    },
+    purchasedCourses :[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'Course'
+    }],
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title : {
+        type:String,
+        required:true,
+        // unique:true
+    },
+    description:{
+        type:String,
+        required:true,
+        // unique:true
+    },
+    price:{
+        type:Number,
+        required:true,
+    },
+    imageLink:{
+        type:String,
+        required:true
+    },
+    published:{
+        type:Boolean,
+        required:true,
+    },
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
--- week-3/04-mongo-with-jwt-auth/index.js
+++ week-3/04-mongo-with-jwt-auth/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+require('dotenv').config();
 const adminRouter = require("./routes/admin")
 const userRouter = require("./routes/user");
 
@@ -10,5 +11,5 @@ app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
 app.listen(3000, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port 3000`);
 });
