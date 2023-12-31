6003fcbed552b18f3b767bb3188e18359a7c0906
“[Akshit <akshitlakhera14@gmail.com>
2023-12-31 12:27:59 +0530

    Created basic todo application

--- week-4/react-basics/todo application/src/App.jsx
+++ week-4/react-basics/todo application/src/App.jsx
@@ -1,35 +1,100 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+export default App
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState({
+      id: Date.now(), 
+      title: "",
+      description: ""
+  });
+
+  function handleChange(e) {
+      const { value, name } = e.target;
+      setTodo({
+          ...todo,
+          [name]: value
+      })
+  }
+
+  function handleSubmit(e) {
+      e.preventDefault();
+      setTodos(prev => [...prev, todo]);
+      setTodo({ id: Date.now(), title: "", description: "" }); 
+  }
+
+  function handleDelete(id) {
+      const updatedTodos = todos.filter(todo => todo.id !== id);
+      setTodos(updatedTodos);
+  }
+
+  function handleUpdate(id) {
+      const newTitle = prompt("Enter your new title")
+      const newDescription = prompt("Enter new deacription")
+
+      const updatedTodos = todos.map(todo => {
+          if(todo.id === id) {
+              todo.title = newTitle
+              todo.description = newDescription
+          }
+          return todo;
+      })
+
+      setTodos(updatedTodos)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <>
+          <form onSubmit={handleSubmit}>
+              <input type="text" name="title" value={todo.title} onChange={handleChange} /> <br /> <br />
+              <input type="text" name="description" value={todo.description} onChange={handleChange} /> <br /> <br />
+              <button type='submit'>submit</button>
+          </form>
 
-export default App
+          {todos && todos.map((todo) => {
+              return (
+                  <div className='todoStyle' key={todo.id}>
+                      <p>{todo.title}</p>
+                      <p>{todo.description}</p>
+                      <button onClick={() => handleUpdate(todo.id)}>Update</button>
+                      <button onClick={() => handleDelete(todo.id)}>Delete</button>
+                  </div>
+              )
+          })}
+      </>
+  )
+//   const [titlevalue,settitle]=useState('');
+//   const [descriptionvalue,setdescriptionvalue]=useState('');
+//   const [tasks,setTasks] = useState([]);
+//   const handleclick = () => {
+//     setTasks([...tasks,{title :titlevalue ,description:descriptionvalue }])
+//     settitle('');
+//     setdescriptionvalue('');
+//   }
+//   const handletitle = (event) => {
+//     settitle(event.target.value)
+//   }
+//   const handledescription = (event) => {
+//     setdescriptionvalue(event.target.value)
+//   }
+
+//   return (
+//     <div>
+//     <input type="text" placeholder='Title' 
+//     value={titlevalue} 
+//     onChange={handletitle}/>
+//     <br></br>
+//     <input type= "text" placeholder='description'
+//     value={descriptionvalue}
+//     onChange={handledescription}/>
+//     <br></br>
+//     <button onClick={handleclick}>Add task</button>
+//      {tasks.map((task,index) => (
+//     <div  key={index}>
+//       <p> title :{task.title}  </p>
+//       <p> description : {task.description}  </p>
+//     </div>
+//      ))
+// }
+// </div> )
+}
