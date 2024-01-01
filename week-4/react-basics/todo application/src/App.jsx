cbfb6475a18465c8307d5c3ca564598b9ca86dc7
“[Akshit <akshitlakhera14@gmail.com>
2024-01-01 17:59:24 +0530

    Done with todo application code

--- week-4/react-basics/todo application/src/App.jsx
+++ week-4/react-basics/todo application/src/App.jsx
@@ -2,99 +2,172 @@ import { useState } from 'react'
 import './App.css'
 export default App
 function App() {
-  const [todos, setTodos] = useState([]);
-  const [todo, setTodo] = useState({
-      id: Date.now(), 
-      title: "",
-      description: ""
-  });
+  const [titlevalue,settitle]=useState('');
+  const [descriptionvalue,setdescriptionvalue]=useState('');
+  const [tasks,setTasks] = useState([]);
+  const [editingTask, setEditingTask] = useState(null);
+  const handleSubmit = (e) => {
+        e.preventDefault();
+        setTasks([
+          ...tasks,
+          {
+            id: Date.now(),
+            title: titlevalue,
+            description: descriptionvalue,
+          },
+        ]);
+        settitle('');
+        setdescriptionvalue('');
+      };
+    
 
-  function handleChange(e) {
-      const { value, name } = e.target;
-      setTodo({
-          ...todo,
-          [name]: value
-      })
+  const handletitle = (event) => {
+    settitle(event.target.value)
   }
-
-  function handleSubmit(e) {
-      e.preventDefault();
-      setTodos(prev => [...prev, todo]);
-      setTodo({ id: Date.now(), title: "", description: "" }); 
+  const handledescription = (event) => {
+    setdescriptionvalue(event.target.value)
   }
-
-  function handleDelete(id) {
-      const updatedTodos = todos.filter(todo => todo.id !== id);
-      setTodos(updatedTodos);
-  }
-
-  function handleUpdate(id) {
-      const newTitle = prompt("Enter your new title")
-      const newDescription = prompt("Enter new deacription")
-
-      const updatedTodos = todos.map(todo => {
-          if(todo.id === id) {
-              todo.title = newTitle
-              todo.description = newDescription
-          }
-          return todo;
-      })
-
-      setTodos(updatedTodos)
-  }
-
+  const handleDelete = (taskId) => {
+        setTasks(tasks.filter((task) => task.id !== taskId));
+      };
+  const handleEdit = (task) => {
+  setEditingTask(task)
+ }
+ const handleSave = () => {
+  const updatedTasks = tasks.map((task) =>
+    task.id === editingTask.id ? editingTask : task
+  );
+  setTasks(updatedTasks);
+  setEditingTask(null);
+};
   return (
-      <>
-          <form onSubmit={handleSubmit}>
-              <input type="text" name="title" value={todo.title} onChange={handleChange} /> <br /> <br />
-              <input type="text" name="description" value={todo.description} onChange={handleChange} /> <br /> <br />
-              <button type='submit'>submit</button>
-          </form>
+    <div>
+        <form  onSubmit={handleSubmit}>
+    <input type="text" placeholder='Title' 
+    value={titlevalue} 
+    onChange={handletitle}/>
+    <br></br>
+    <input type= "text" placeholder='description'
+    value={descriptionvalue}
+    onChange={handledescription}/>
+    <br></br>
+    <button type='submit'>Add task</button>
+    </form>
+     {tasks.map((task) => (
+    <div  key={task.id}>
+      <p> title :{task.title}  </p>
+      <p> description : {task.description}  </p>
+      <button onClick={() => {handleDelete(task.id)  }}>Delete</button>
+      {editingTask?.id  !== task.id &&
+      ( <button onClick={() => handleEdit(task)}>Edit</button>)
+      }
+      {editingTask?.id === task.id && (
+        <div>
+          <input
+          type='text'
+          placeholder='edit titile'
+          onChange={(e) => setEditingTask({ ...editingTask, title: e.target.value })}
+          value={editingTask.title}/>
+          <input
+                type="text"
+                value={editingTask.description}
+                onChange={(e) => setEditingTask({ ...editingTask, description: e.target.value })}
+              />
+               <button onClick={handleSave}>Save</button>
+        </div>
+      )}
+    </div>
+     ))
+}
+</div> )
+// const [taskTitle, setTaskTitle] = useState('');
+//   const [taskDescription, setTaskDescription] = useState('');
+//   const [tasks, setTasks] = useState([]);
+//   const [editingTask, setEditingTask] = useState(null);
 
-          {todos && todos.map((todo) => {
-              return (
-                  <div className='todoStyle' key={todo.id}>
-                      <p>{todo.title}</p>
-                      <p>{todo.description}</p>
-                      <button onClick={() => handleUpdate(todo.id)}>Update</button>
-                      <button onClick={() => handleDelete(todo.id)}>Delete</button>
-                  </div>
-              )
-          })}
-      </>
-  )
-//   const [titlevalue,settitle]=useState('');
-//   const [descriptionvalue,setdescriptionvalue]=useState('');
-//   const [tasks,setTasks] = useState([]);
-//   const handleclick = () => {
-//     setTasks([...tasks,{title :titlevalue ,description:descriptionvalue }])
-//     settitle('');
-//     setdescriptionvalue('');
-//   }
-//   const handletitle = (event) => {
-//     settitle(event.target.value)
-//   }
-//   const handledescription = (event) => {
-//     setdescriptionvalue(event.target.value)
-//   }
+//   const handleTaskSubmit = (e) => {
+//     e.preventDefault();
+//     setTasks([
+//       ...tasks,
+//       {
+//         id: Date.now(),
+//         title: taskTitle,
+//         description: taskDescription,
+//       },
+//     ]);
+//     setTaskTitle('');
+//     setTaskDescription('');
+//   };
+
+//   const handleTitleChange = (e) => {
+//     setTaskTitle(e.target.value);
+//   };
+
+//   const handleDescriptionChange = (e) => {
+//     setTaskDescription(e.target.value);
+//   };
+
+//   const handleDelete = (taskId) => {
+//     setTasks(tasks.filter((task) => task.id !== taskId));
+//   };
+
+//   const handleEdit = (task) => {
+//     setEditingTask(task);
+//   };
+
+//   const handleSave = () => {
+//     const updatedTasks = tasks.map((task) =>
+//       task.id === editingTask.id ? editingTask : task
+//     );
+//     setTasks(updatedTasks);
+//     setEditingTask(null);
+//   };
 
 //   return (
 //     <div>
-//     <input type="text" placeholder='Title' 
-//     value={titlevalue} 
-//     onChange={handletitle}/>
-//     <br></br>
-//     <input type= "text" placeholder='description'
-//     value={descriptionvalue}
-//     onChange={handledescription}/>
-//     <br></br>
-//     <button onClick={handleclick}>Add task</button>
-//      {tasks.map((task,index) => (
-//     <div  key={index}>
-//       <p> title :{task.title}  </p>
-//       <p> description : {task.description}  </p>
+//       <form onSubmit={handleTaskSubmit}>
+//         <input
+//           type="text"
+//           placeholder="Title"
+//           value={taskTitle}
+//           onChange={handleTitleChange}
+//         />
+//         <br />
+//         <input
+//           type="text"
+//           placeholder="Description"
+//           value={taskDescription}
+//           onChange={handleDescriptionChange}
+//         />
+//         <br />
+//         <button type="submit">Add Task</button>
+//       </form>
+
+//       {tasks.map((task) => (
+//         <div key={task.id}>
+//           <p>Title: {task.title}</p>
+//           <p>Description: {task.description}</p>
+//           <button onClick={() => handleDelete(task.id)}>Delete</button>
+//           {editingTask?.id !== task.id && (
+//             <button onClick={() => handleEdit(task)}>Edit</button>
+//           )}
+//           {editingTask?.id === task.id && (
+//             <div>
+//               <input
+//                 type="text"
+//                 value={editingTask.title}
+//                 onChange={(e) => setEditingTask({ ...editingTask, title: e.target.value })}
+//               />
+//               <input
+//                 type="text"
+//                 value={editingTask.description}
+//                 onChange={(e) => setEditingTask({ ...editingTask, description: e.target.value })}
+//               />
+//               <button onClick={handleSave}>Save</button>
+//             </div>
+//           )}
+//         </div>
+//       ))}
 //     </div>
-//      ))
-// }
-// </div> )
+//   );
 }
