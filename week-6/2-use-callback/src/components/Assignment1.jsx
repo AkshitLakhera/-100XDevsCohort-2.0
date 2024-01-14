f5581c4cacefc2a21a898adf815779baa752911c
“[Akshit <akshitlakhera14@gmail.com>
2024-01-14 11:57:28 +0530

    Done with adding userCallback logic

--- week-6/2-use-callback/src/components/Assignment1.jsx
+++ week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
@@ -6,22 +6,22 @@ export function Assignment1() {
     const [count, setCount] = useState(0);
 
     // Your code starts here
-    function handleIncrement() {
 
-    }
-
-    function handleDecrement() {
-        
-    }
-    // Your code ends here
-
+const handleIncrement  = useCallback(()  => {
+                                    return setCount(count +1);
+    },[count]
+)
+const handleDecrement = useCallback(()  => {
+    return setCount(count -1);
+},[count]
+)
     return (
         <div>
             <p>Count: {count}</p>
             <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
         </div>
     );
-};
+}
 
 const CounterButtons = ({ onIncrement, onDecrement }) => (
     <div>
