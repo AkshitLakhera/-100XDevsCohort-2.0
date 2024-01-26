d2edd18b0bc0f7986a5f7d024af61adc2c26a738
“[Akshit <akshitlakhera14@gmail.com>
2024-01-26 18:07:20 +0530

    Removed that temlaplate will work from myself

--- week-7(new)/Git-Finder/git-finder-app/src/components/Profile.jsx
+++ week-7(new)/Git-Finder/git-finder-app/src/components/Profile.jsx
@@ -1,38 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const Profile = () => {
   return (
-    <section className="text-gray-600 body-font">
-    <div className="container px-5 py-24 mx-auto flex flex-wrap">
-      <div className="flex flex-wrap -mx-4 mt-auto mb-auto lg:w-1/2 sm:w-2/3 content-start sm:pr-10">
-        <div className="w-full sm:p-4 px-4 mb-6">
-          <h1 className="title-font font-medium text-xl mb-2 text-gray-900">Moon hashtag pop-up try-hard offal truffaut</h1>
-          <div className="leading-relaxed">Pour-over craft beer pug drinking vinegar live-edge gastropub, keytar neutra sustainable fingerstache kickstarter.</div>
-        </div>
-        <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-          <h2 className="title-font font-medium text-3xl text-gray-900">2.7K</h2>
-          <p className="leading-relaxed">Users</p>
-        </div>
-        <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-          <h2 className="title-font font-medium text-3xl text-gray-900">1.8K</h2>
-          <p className="leading-relaxed">Subscribes</p>
-        </div>
-        <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-          <h2 className="title-font font-medium text-3xl text-gray-900">35</h2>
-          <p className="leading-relaxed">Downloads</p>
-        </div>
-        <div className="p-4 sm:w-1/2 lg:w-1/4 w-1/2">
-          <h2 className="title-font font-medium text-3xl text-gray-900">4</h2>
-          <p className="leading-relaxed">Products</p>
-        </div>
-      </div>
-      <div className="lg:w-1/2 sm:w-1/3 w-full rounded-lg overflow-hidden mt-6 sm:mt-0">
-        <img className="object-cover object-center w-full h-full" src="https://unsplash.com/photos/man-in-black-zip-up-jacket-ADSKIn0ScDg" alt="stats" />
-      </div>
-    </div>
-  </section>
+    <section className=' text-center  '>
+      <div className='searchBox flex justify-center  '>
+        <input  className="rounded-sm border-2 border-black'" type="text" placeholder="Search..." id="search-input" />
+      <button className="rounded-md border border-transparent px-3 py-2 text-sm font-semibold bg-blue-800 cursor-pointer transition duration-250 hover:border-blue-500 ml-6" type="button" id="search-button ">Search</button></div>
+      <div className='card'>
+        
 
+      </div>
+    </section>
   )
-}
+};
 
-export default Profile
+export default Profile;
