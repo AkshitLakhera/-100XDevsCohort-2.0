b886b8bbc212392ca04ac25bd74837425a04aaa1
“[Akshit <akshitlakhera14@gmail.com>
2024-01-27 23:35:30 +0530

    Basic styles added

--- week-7(new)/week-7 questions/background-changer/src/components/Bgchanger.jsx
+++ week-7(new)/week-7 questions/background-changer/src/components/Bgchanger.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
+import '../App.css'; 
 
 export const Bgchanger = () => {
   return (
-    <div className="card">
-        <button>Red</button>
-        <button>Yellow</button>
-        <button>Black</button>
-        <button>Purple</button>
-        <button>Green</button>
-        <button>Blue</button>
-        <button>Default</button>
+    <div className="whole_page">
+    <div className="card  gap-28">
+        <button className=' btn bg-red-600'>Red</button>
+        <button className=' btn bg-yellow-600'>Yellow</button>
+        <button className= ' btn bg-neutral-950  text-white'>Black</button>
+        <button className=' btn bg-violet-600' >Purple</button>
+        <button className='btn bg-green-600'>Green</button>
+        <button className='btn bg-blue-600' >Blue</button>
+        <button className='btn bg-pink-600'>Default</button>
 
+    </div>
     </div>
   )
 }
