40168713a57d2cac8da20246b8d84b063f93abce
“[Akshit <akshitlakhera14@gmail.com>
2024-01-27 22:57:57 +0530

    Added markup

---
+++ week-7(new)/week-7 questions/background-changer/src/components/Bgchanger.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Bgchanger = () => {
+  return (
+    <div className="card">
+        <button>Red</button>
+        <button>Yellow</button>
+        <button>Black</button>
+        <button>Purple</button>
+        <button>Green</button>
+        <button>Blue</button>
+        <button>Default</button>
+
+    </div>
+  )
+}
