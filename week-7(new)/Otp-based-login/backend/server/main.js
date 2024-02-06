51413d5bc05e8205a9fca2d00f5f83ec8a850f30
“[Akshit <akshitlakhera14@gmail.com>
2024-02-06 13:02:39 +0530

    Added env file

--- week-7(new)/Otp-based-login/backend/server/main.js
+++ week-7(new)/Otp-based-login/backend/server/main.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 const twilio = require('twilio');
 
 const app = express();
@@ -9,11 +10,14 @@ const port = 5000;
 
 app.use(bodyParser.json());
 
+
+
 // Twilio credentials (replace with your own credentials)
 const accountSid = 'AC96a732a9eafe549f0ab0eef134160ab0';
-const authToken = process.env.TWILIO_AUTH_TOKEN
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioClient = twilio(accountSid, authToken);
-const twilioPhoneNumber = '9758584612';
+const twilioPhoneNumber = process.env.PHN_NUMBER;
+console.log(process.env.TWILIO_AUTH_TOKEN);
 // Generate a random  6 digit otp
 const generateOTP = () => {
     return Math.floor(100000 + Math.random() * 900000).toString(); // added 100000 as we want the six digit number
@@ -22,16 +26,17 @@ const generateOTP = () => {
 const otpStorage = {};
 
 // Endpoint to send otp ;
+
 app.post('/api/send-otp', async(req,res) => {
     const { phoneNumber } = req.body; // const phoneNumber = req.body.phoneNumber;
     try {
         // generate otp
          const otp = generateOTP();
 
-        //  send otp via twillo sms
+        //  send otp via twillo { '+1234567890': '987654' } sms
         await twilioClient.messages.create({
             body:`Your otp is ${otp}`,
-            from:9758584612,
+            from:twilioPhoneNumber,
             to:phoneNumber,
 
         })
@@ -47,7 +52,19 @@ app.post('/api/send-otp', async(req,res) => {
     }
 );
 
+// Endpoint to verify the OTP
 
-app.listen(3000, () => {
-    console.log(`Server is running at http://localhost:3000`);
+app.post('/api/verify-otp',(req,res) => {
+    const {phoneNumber,otp}=req.body;
+    //  retrieved stored otp
+    const storeOtp = otpStorage[phoneNumber];
+    if(storeOtp && storeOtp === otp) {
+        res.status(200).json({success:true,message:"Otp verification successful"})
+    } else{
+        res.status(400).json({success:false, message:'Invalid OTP'})
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
