f8431bf00e7fd0285e9602a54b62f9cf0b822358
“[Akshit <akshitlakhera14@gmail.com>
2024-02-23 11:33:00 +0530

    pg libraries and setup is ds done

--- week-10/1-postgres-simple/src/config.ts
+++ week-10/1-postgres-simple/src/config.ts
@@ -1,2 +1,2 @@
-
-export const DB_URL = "postgres://postgres:mysecretpassword@localhost/postgres";
+//I have added the my database url
+export const DB_URL = "postgresql://akshitlakhera14:JP8yNLfF6lUH@ep-summer-mouse-a58lw563.us-east-2.aws.neon.tech/firstneonDb?sslmode=require";
