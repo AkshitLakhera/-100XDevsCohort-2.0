a058270631f22a29e47838119e40ada197ccde24
“[Akshit <akshitlakhera14@gmail.com>
2024-02-23 12:08:38 +0530

    Added code for Inserting  user details in backend

--- week-10/1-postgres-simple/src/db/user.ts
+++ week-10/1-postgres-simple/src/db/user.ts
@@ -10,7 +10,18 @@ import { client } from "..";
  * }
  */
 export async function createUser(username: string, password: string, name: string) {
-    
+    try {
+        await client.connect();  // to get connection
+        const insertQuery = `INSERT INTO  users ("username","password","name") VALUES ($1,$2,$3)`;
+        const values = [username,password,name];
+        const res    =  await client.query(insertQuery,values);
+        console.log("insertion success",res)    //output inseted result    
+}
+catch(error) {
+    console.error("Error during insertion",error)
+} finally {
+    await client.end();
+}
 }
 
 /*
